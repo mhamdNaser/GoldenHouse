43b2d7f154e11ac1bc6a20802bf8afdd7f7ce213
mhamdNaser <115448369+mhamdNaser@users.noreply.github.com>
2023-03-22 14:19:22 +0300

    other update

--- resources/views/admin/massage/thanksMassage.blade.php
+++ resources/views/admin/massage/thanksMassage.blade.php
@@ -5,13 +5,15 @@
 <div class="mt-4">
     @section('content')
     <!-- Contact-->
-    <div class="row pt-4 ps-2 pe-2 d-flex " style="background-image: url({{url('/images/contact-bg.jpg')}}); background-size: cover; background-position: center center;">
-        <div class="col-lg-6 p-5">
-            <div class="card-body py-5 px-md-5 m-5 myopacity rounded-3">
-                <h3>Thanks For Your Massage</h3>
+
+        <div class="d-flex py-5 ps-2 pe-2" style="background-image: url({{url('/images/contact-bg.jpg')}}); background-size: cover; background-position: center center;">
+            <div class="container pt-5 pb-5 m-5">
+            <div class="col-lg-6 pt-5 pb-5">
+                <div class="card-body py-5 px-md-5 m-5 myopacity rounded-3">
+                    <h3>Thanks For Your Massage</h3>
+                </div>
             </div>
         </div>
-       
     </div>
   <!-- Section: Design Block -->
 @endsection
